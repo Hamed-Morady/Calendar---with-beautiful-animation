395390e6df71effed711867453d190f4ca29bcab
Hamed_Morady <165544770+Hamed-Morady@users.noreply.github.com>
2025-10-23 21:57:51 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import Calendar from "./Calendar";
 
 function App() {
-  const now = new Date();
   return <Calendar/>;
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
